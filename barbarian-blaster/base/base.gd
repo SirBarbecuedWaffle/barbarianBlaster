a8b3d48134dad577b6271f93ab8e6934418580e8
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-01-20 13:08:53 -0600

    colorLabel

--- barbarian-blaster/base/base.gd
+++ barbarian-blaster/base/base.gd
@@ -6,10 +6,15 @@ var health:=0
 func _ready() -> void:
 	health=maxHealth
 	label_3d.text=str(health)+"/"+str(maxHealth)
+	label_3d.modulate=Color("white")
 	
 func take_damage()->void:
 	print("damage taken by base")
 	set_health(health-1)
+	var red=Color.RED
+	var white=Color.WHITE
+	var color=red.lerp(label_3d.modulate,0.7)
+	label_3d.modulate=color
 
 func set_health(new_health : int)->void:
 	health=new_health
