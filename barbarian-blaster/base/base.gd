cfad471e0f46c9af87dc364d9310655a6a27669a
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-01-20 12:32:18 -0600

    health depletion reset works

--- barbarian-blaster/base/base.gd
+++ barbarian-blaster/base/base.gd
@@ -1,4 +1,18 @@
 extends Node3D
 class_name Base
+@onready var label_3d: Label3D = $Label3D
+@export var maxHealth:=5
+var health:=0
+func _ready() -> void:
+	health=maxHealth
+	label_3d.text=str(health)+"/"+str(maxHealth)
+	
 func take_damage()->void:
 	print("damage taken by base")
+	set_health(health-1)
+
+func set_health(new_health : int)->void:
+	health=new_health
+	label_3d.text=str(health)+"/"+str(maxHealth)
+	if health<0:
+		get_tree().reload_current_scene()
