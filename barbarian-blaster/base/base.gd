51c760b29cbb944f96ea1e00058f8566873b1b13
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-01-21 12:19:47 -0600

    betterHealthLabel

--- barbarian-blaster/base/base.gd
+++ barbarian-blaster/base/base.gd
@@ -1,23 +1,22 @@
 extends Node3D
 class_name Base
 @onready var label_3d: Label3D = $Label3D
-@export var maxHealth:=5
-var health:=0
+@export var maxHealth:=5.0
+var health:=0.0
 func _ready() -> void:
 	health=maxHealth
-	label_3d.text=str(health)+"/"+str(maxHealth)
+	label_3d.text=str(int(health))+"/"+str(int(maxHealth))
 	label_3d.modulate=Color("white")
 	
 func take_damage()->void:
 	print("damage taken by base")
 	set_health(health-1)
-	var red=Color.RED
-	var white=Color.WHITE
-	var color=red.lerp(label_3d.modulate,0.7)
-	label_3d.modulate=color
 
 func set_health(new_health : int)->void:
 	health=new_health
-	label_3d.text=str(health)+"/"+str(maxHealth)
-	if health<0:
+	label_3d.text=str(int(health))+"/"+str(int(maxHealth))
+	var red=Color.RED
+	var white=Color.WHITE
+	label_3d.modulate=red.lerp(white,(health/maxHealth))
+	if health<1:
 		get_tree().reload_current_scene()
