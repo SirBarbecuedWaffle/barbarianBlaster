00dba2f542284e42d9b480f7eba6c5bc2016ecb3
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-01-27 12:39:23 -0600

    difficulty scaling

--- barbarian-blaster/enemy/path_3d.gd
+++ barbarian-blaster/enemy/path_3d.gd
@@ -1,12 +1,14 @@
 extends Path3D
 @export var spawnedEnemy : PackedScene
 @onready var enemy_spawn_timer: Timer = $enemySpawnTimer
-
+@export var difficultyManager:Node
 
 func spawnEnemy()->void:
 	var newEnemy=spawnedEnemy.instantiate()
 	add_child(newEnemy)
 	newEnemy.progress=0
+	newEnemy.max_health=difficultyManager.get_enemy_health()
+	enemy_spawn_timer.wait_time=difficultyManager.get_spawn_time()
 
 
 func _on_enemy_spawn_timer_timeout() -> void:
