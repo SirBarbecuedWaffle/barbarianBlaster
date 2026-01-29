119cf3a33470157cee41d9c80d6cd7ceaaab83fa
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-01-29 12:32:51 -0600

    turrets look good

--- barbarian-blaster/turret/turret.gd
+++ barbarian-blaster/turret/turret.gd
@@ -1,16 +1,18 @@
 extends Node3D
 var proj=preload("res://projectile/projectile.tscn")
 @export var enemyPath : Path3D
+@onready var cannon: Node3D = $turretBase/turretTop/Cannon
 var target : Node3D
 @export var range:=10.0
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var turret_base: MeshInstance3D = $turretBase
+@onready var turret_base: Node3D = $turretBase
 @onready var timer: Timer = $Timer
+@onready var turret_mesh: Node3D = $turretBase/TurretMesh
 
 func _physics_process(delta: float) -> void:
 	target=find_best_target()
 	if target is Enemy:
-		look_at(target.global_position,Vector3.UP,true)
+		turret_base.look_at(target.global_position,Vector3.UP,true)
 	turret_base.rotation.z=0
 	turret_base.rotation.x=0
 func _on_timer_timeout() -> void:
@@ -18,9 +20,9 @@ func _on_timer_timeout() -> void:
 		animation_player.play("fire")
 		var shotproj=proj.instantiate()
 		add_child(shotproj)
-		shotproj.global_position=global_position
-		shotproj.global_position.y=global_position.y+1.8
-		shotproj.direction=global_transform.basis.z
+		shotproj.global_position=cannon.global_position
+		shotproj.global_position.y=cannon.global_position.y
+		shotproj.direction=cannon.global_transform.basis.z
 
 func find_best_target()->Enemy:
 	var bestEnemy : PathFollow3D = null
